434a3e052020fba7bb617386c83e55e738edb237
matty <mattreed1890@gmail.com>
2025-01-20 20:39:49 -0600

    bind buffer

--- src/main.cpp
+++ src/main.cpp
@@ -23,6 +23,9 @@ int main() {
 	unsigned int VBO;
 	glGenBuffers(1, &VBO);
 
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
 	while (!glfwWindowShouldClose(window)) {
 		processInput(window);
 
